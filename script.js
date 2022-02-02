07efb783cf7633a79a9232d7d6ce2d16f0004344
Arpit <111arpit1@gmail.com>
2022-02-02 18:45:57 +0530

    update

---
+++ script.js
@@ -0,0 +1,50 @@
+//NavBar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// scroll to top starts
+var scrolltop = $("#scroll-top");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 80) {
+    scrolltop.addClass("active");
+  } else {
+    scrolltop.removeClass("active");
+  }
+});
+
+scrolltop.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "200");
+});
+// scroll to top ends
+
+fetch("style_links.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let links = data;
+
+    for (let i = 0; i < links.length; i++) {
+      console.log(links.length);
+      let StyleBoxDiv = document.createElement("div");
+      StyleBoxDiv.innerHTML = `<div class="stylebox">
+            <div class="image">
+                 <img src="https://avatars.githubusercontent.com/${links[i].Github_username}" alt="">
+             </div>
+            <p>${links[i].name}</p>
+            
+            <div class="btn-group">
+            <a target="_blank" href="https://github.com/${links[i].Github_username}">View Profile</a>
+
+            </div>
+
+            </div>`;
+      document.getElementById("styles").appendChild(StyleBoxDiv);
+    }
+  });
